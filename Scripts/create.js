80f78dce76503422a215ace20cf8dcf5e98d63d4
DavidTrannam <trannamd@oregonstate.edu>
2021-06-08 00:18:23 -0700

    Planning

--- Scripts/create.js
+++ Scripts/create.js
@@ -3,5 +3,8 @@ selectedDays = document.querySelector('#days')
 
 selectedDays.addEventListener('input', () => {
     start = 0
+    // remove all elemenets of class x 
 
+    // recreate all elements of class x and provide ID based on start
+    
 })
